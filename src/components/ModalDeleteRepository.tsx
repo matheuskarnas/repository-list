4f321e70149fcf3f839dfa44a08c437a27694668
matheuskarnas <matheuskarnas1@gmail.com>
2022-06-16 19:12:45 -0300

    Delete confirmation modal created

---
+++ src/components/ModalDeleteRepository.tsx
@@ -0,0 +1,55 @@
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay
+} from "@chakra-ui/react"
+import axios from "axios";
+import { useRouter } from "next/router";
+import { RepositoryDataProps } from "../pages";
+
+type ModalDeleteRepositoryProps = {
+    onOpen: () => void;
+    onClose: () => void;
+    isOpen: boolean;
+    repository: RepositoryDataProps
+
+}
+
+export const ModalDeleteRepository = ({ onOpen, isOpen, onClose, repository }: ModalDeleteRepositoryProps) => {
+    const router = useRouter()
+    const reloadPage = () => router.reload()
+    async function deleteRepository(id: string) {
+        await axios.post(`/api/deleteRepository`, { id }).then(onClose).then(reloadPage)
+
+    }
+
+    return (
+        <>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Deseja realmente excluir o repositório</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <h1>Nome do repositório: {repository.title}</h1>
+                        <h2>Link:
+
+                        </h2>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={() => deleteRepository(repository.id)}>
+                            Excluir
+                        </Button>
+                        <Button onClick={onClose} variant='ghost'>Cancelar</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
