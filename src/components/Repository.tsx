386d06b52137731422c7e626154c102c2f5f8f7e
matheuskarnas <matheuskarnas1@gmail.com>
2022-06-15 21:40:52 -0300

    added DeleteIcon functionally

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -1,14 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { Box, HStack } from "@chakra-ui/react";
+import axios from "axios";
 import { RepositoryDataProps } from "../pages";
 
-type RepositoryProps = { 
+type RepositoryProps = {
     key: string;
-    repository: RepositoryDataProps 
+    repository: RepositoryDataProps
 }
 
-export const Repository = ({ repository }: RepositoryProps) => (
-    <Box>
-        <h1>{repository.title}</h1>
-        <h2>{repository.url}</h2>
-    </Box>
-)
+export const Repository = ({ repository }: RepositoryProps) => {
+
+    async function deleteRepository(id: string) {
+        axios.post(`/api/deleteRepository`, { id })
+    }
+
+    return (
+        <HStack border={'1px'} m={'0.5px 0'} >
+            <Box>
+                <h1>{repository.title}</h1>
+                <h2>{repository.url}</h2>
+            </Box>
+            <DeleteIcon
+                h={7}
+                w={7}
+                ml={'auto'}
+                onClick={() => deleteRepository(repository.id)}
+            />
+        </HStack>
+    )
+}
