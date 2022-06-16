06d6c2e062aaa75fccdad567888dad2e3aa1e704
matheuskarnas <matheuskarnas1@gmail.com>
2022-06-16 19:13:47 -0300

    implemented ModalDeleteRepository

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -1,7 +1,8 @@
 import { DeleteIcon } from "@chakra-ui/icons";
-import { Box, HStack } from "@chakra-ui/react";
-import axios from "axios";
+import { Box, HStack, useDisclosure } from "@chakra-ui/react";
+import Link from "next/link";
 import { RepositoryDataProps } from "../pages";
+import { ModalDeleteRepository } from "./ModalDeleteRepository";
 
 type RepositoryProps = {
     key: string;
@@ -9,23 +10,27 @@ type RepositoryProps = {
 }
 
 export const Repository = ({ repository }: RepositoryProps) => {
-
-    async function deleteRepository(id: string) {
-        axios.post(`/api/deleteRepository`, { id })
-    }
+    const { onOpen, isOpen, onClose } = useDisclosure()
 
     return (
-        <HStack border={'1px'} m={'0.5px 0'} >
-            <Box>
-                <h1>{repository.title}</h1>
-                <h2>{repository.url}</h2>
-            </Box>
-            <DeleteIcon
-                h={7}
-                w={7}
-                ml={'auto'}
-                onClick={() => deleteRepository(repository.id)}
-            />
-        </HStack>
+        <>
+            <ModalDeleteRepository repository={repository} onOpen={onOpen} isOpen={isOpen} onClose={onClose} />
+            <HStack border={'1px'} m={'0.5px 0'} >
+
+                <Box>
+                    <h1>{repository.title}</h1>
+                    <Link href={repository.url}>
+                        <a target='_blank' >{repository.url}</a>
+                    </Link>
+                </Box>
+
+                <DeleteIcon
+                    h={7}
+                    w={7}
+                    ml={'auto'}
+                    onClick={onOpen}
+                />
+            </HStack>
+        </>
     )
 }
